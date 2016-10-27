069c239559106420f52986ec2d3b0d2caa41ada3
Toph Allen <toph.allen@gmail.com>
2016-10-27 16:05:59 -0400

    Pubcrawler plots updated

--- inst/scripts/figures_etc/pubcrawler_plots.R
+++ inst/scripts/figures_etc/pubcrawler_plots.R
@@ -24,6 +24,15 @@ prettypubs <- pubs_superhires %>%
 
 numcolors <- length(unique(prettypubs[["z"]]))
 
+clip_at_sd <- function(x, multiple = 1) {
+  m <- mean(x, na.rm = TRUE)
+  s <- sd(x, na.rm = TRUE) * multiple
+  y <- x %>%
+    pmin(m + s) %>%
+    pmax(m - s)
+  return(y)
+}
+
 ggplot() +
   geom_polygon(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, fill = "#440154FF") +
   geom_raster(aes(x = x, y = y, fill = quantvar(z)), data = prettypubs) + 
@@ -34,8 +43,31 @@ ggplot() +
   theme_black_legend() +
   labs(x = NULL, y = NULL)
 
-ggsave(file.path(current_out_dir, "pubcrawler_hires.pdf"), height = 4.5, width = 9)
-ggsave(file.path(current_out_dir, "pubcrawler_hires.png"), height = 4.5, width = 9)
+ggsave(file.path(current_out_dir, "pubcrawler_hires_quantvar.pdf"), height = 4.5, width = 9)
+ggsave(file.path(current_out_dir, "pubcrawler_hires_quantvar.png"), height = 4.5, width = 9)
+
+ggplot() +
+  geom_polygon(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, fill = "#440154FF") +
+  geom_raster(aes(x = x, y = y, fill = winsorize(z, 0.75)), data = prettypubs) + 
+  geom_path(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, color = "white", size = 0.15) +
+  coord_fixed() +
+  scale_fill_gradientn(colours = viridis(numcolors),
+                       guide = guide_colorbar(title = "Predicted\nPublications")) +
+  theme_black_legend() +
+  labs(x = NULL, y = NULL)
+
+ggplot() +
+  geom_polygon(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, fill = "#440154FF") +
+  geom_raster(aes(x = x, y = y, fill = pmin(z, quantile(z, probs = 0.9))), data = prettypubs) + 
+  geom_path(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, color = "white", size = 0.15) +
+  coord_fixed() +
+  scale_fill_gradientn(colours = viridis(numcolors),
+                       guide = guide_colorbar(title = "Predicted\nPublications")) +
+  theme_black_legend() +
+  labs(x = NULL, y = NULL)
+
+ggsave(file.path(current_out_dir, "pubcrawler_hires_truncated.pdf"), height = 4.5, width = 9)
+ggsave(file.path(current_out_dir, "pubcrawler_hires_truncated.png"), height = 4.5, width = 9)
 
 quickmap(prettypubs, quantvar(z), pal_fun = "viridis") +
   geom_polygon(aes(x = long, y = lat, group = group), data = map_data("world"), inherit.aes = FALSE, color = "white", fill = "#440154FF", size = 0.1) +
