2377d3f97a734e48da9007c0ef9e45ccdc5e3d16
Toph Allen <toph.allen@gmail.com>
2016-11-05 20:23:08 +0100

    Publications plots for presentation

---
+++ inst/scripts/figures_etc/variable_maps.R
@@ -0,0 +1,79 @@
+load_all()
+
+current_out_dir <- file.path(out_dir(), "additional_figures")
+dir.create(current_out_dir, showWarnings = FALSE)
+
+library(ggplot2)
+library(GGally)
+library(dplyr)
+library(viridis)
+
+load_all()
+
+load("cache/drivers_orig.RData")
+data(drivers)
+
+pubs <- drivers %>%
+  select(gridid, lon, lat, iso3, pubs_fit) %>%
+  left_join(select(drivers_orig, gridid, pubs_jones))
+
+quickmap(pubs, pubs_jones)
+quickmap(pubs, pubs_fit)
+
+
+
+clip_at_sd <- function(x, multiple = 1) {
+  m <- mean(x, na.rm = TRUE)
+  s <- sd(x, na.rm = TRUE) * multiple
+  y <- x %>%
+    pmin(m + s) %>%
+    pmax(m - s)
+  return(y)
+}
+
+pretty_jones <- pubs %>%
+  select(x = lon, y = lat, z = pubs_jones) %>%
+  rasterFromXYZ(crs = crs(template_raster())) %>%
+  raster::disaggregate(2, method = "bilinear") %>%
+  mask(mask = country_outlines) %>%
+  as.data.frame(xy = TRUE) %>%
+  na.omit()
+
+numcolors <- length(unique(pretty_jones[["z"]]))
+
+ggplot() +
+  geom_polygon(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, fill = "#440154FF") +
+  geom_raster(aes(x = x, y = y, fill = log(z)), data = pretty_jones) + 
+  geom_path(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, color = "white", size = 0.15) +
+  coord_fixed() +
+  scale_fill_gradientn(colours = viridis(numcolors),
+                       guide = guide_colorbar(title = "Publications")) +
+  theme_black_nothing() +
+  labs(x = NULL, y = NULL)
+
+ggsave(file.path(current_out_dir, "pubs_jones.pdf"), height = 4.5, width = 9)
+ggsave(file.path(current_out_dir, "pubs_jones.png"), height = 4.5, width = 9)
+
+
+pretty_pubcrawler <- pubs %>%
+  select(x = lon, y = lat, z = pubs_fit) %>%
+  rasterFromXYZ(crs = crs(template_raster())) %>%
+  raster::disaggregate(2, method = "bilinear") %>%
+  mask(mask = country_outlines) %>%
+  as.data.frame(xy = TRUE) %>%
+  na.omit()
+
+numcolors <- length(unique(pretty_pubcrawler[["z"]]))
+
+ggplot() +
+  geom_polygon(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, fill = "#440154FF") +
+  geom_raster(aes(x = x, y = y, fill = log(z)), data = pretty_pubcrawler) + 
+  geom_path(aes(x = lon, y = lat, group = group), data = map.world(), inherit.aes = FALSE, color = "white", size = 0.15) +
+  coord_fixed() +
+  scale_fill_gradientn(colours = viridis(numcolors),
+                       guide = guide_colorbar(title = "Publications")) +
+  theme_black_nothing() +
+  labs(x = NULL, y = NULL)
+
+ggsave(file.path(current_out_dir, "pubs_fit.pdf"), height = 4.5, width = 9)
+ggsave(file.path(current_out_dir, "pubs_fit.png"), height = 4.5, width = 9)
