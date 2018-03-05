13f403d1abc0884b86c53e7395c4eb5dce89b2e7
toph-allen <toph.allen@gmail.com>
2018-03-05 14:42:10 -0500

    Updated Yasha export script.

--- inst/scripts/figures_etc/export_for_yasha_2018-03-02.R
+++ inst/scripts/figures_etc/export_for_yasha_2018-03-02.R
@@ -5,6 +5,7 @@ library(gbm)
 library(matrixStats)
 library(purrr)
 library(viridis)
+library(readr)
 
 data(drivers_full)
 
@@ -29,7 +30,9 @@ save(response_matrix, file = file.path(cache_dir(), "bsm_1000_iter", "response_m
 
 load(file.path(cache_dir(), "bsm_1000_iter", "response_matrix.RData"))
 
-bsm_response <- drivers_full %>%
+response_tbl <- response_matrix %>% as.data.frame() %>% as.tbl()
+
+bsm_response_mean <- drivers_full %>%
 	select(gridid, lon, lat) %>%
 	mutate(response_mean = rowMeans(response_matrix),
 	       response_sd = rowSds(response_matrix))
@@ -39,14 +42,17 @@ bsm_response_quantiles = response_matrix %>%
 	as.data.frame()
 names(bsm_response_quantiles) <- c("Q05", "Q25", "Q5", "Q75", "Q95")
 
-bsm_response <- bind_cols(bsm_response, bsm_response_quantiles) %>%
+bsm_response <- bind_cols(bsm_response_mean, bsm_response_quantiles) %>%
 	as.tbl()
 
 # This section needs to be adapted to compute the statistics for the actual layers. Or I'll ask Yasha.
-
-predictions <- drivers_full %>%
+to_export <- drivers_full %>%
   mutate(weight_pubs = pubs_fit / sum(pubs_fit, na.rm = TRUE),
-         weight_pop = pop / sum(pop, na.rm = TRUE),
-         bsm_weight_pubs = bsm_response * weight_pubs,
-         bsm_weight_pop = bsm_response * weight_pop)
+         weight_pop = pop / sum(pop, na.rm = TRUE)) %>%
+  select(weight_pubs, weight_pop) %>%
+  bind_cols(bsm_response)
 
+
+
+write_csv(to_export, path = file.path("inst", "out", "export_for_yasha_2018-03-05","bsm_output.csv"))
+write_csv(response_tbl, path = file.path("inst", "out", "export_for_yasha_2018-03-05","response_tbl.csv"))
